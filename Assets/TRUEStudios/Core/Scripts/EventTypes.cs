f37f54d5f3ac9a00d9cf5778f8f35a0374d4832f
Travis True <travis@travistrue.io>
2018-08-18 13:40:16 -0700

    Updated copyright label for some of the Foundation framework's scripts.

--- Assets/TRUEStudios/Core/Scripts/EventTypes.cs
+++ Assets/TRUEStudios/Core/Scripts/EventTypes.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/******************************************************************************
+ * Foundation Framework
+ * Created by: Travis J True, 2016
+ * This framework is free to use with no limitations.
+******************************************************************************/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
